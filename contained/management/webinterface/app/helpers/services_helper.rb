3c0fd1261fac395fc1de4ee0ca433ca02621c31c
Bram Wijnands <bram@bram-wijnandss-macbook-pro.local>
2009-04-06 17:09:55 +0200

    EOD

--- contained/management/webinterface/app/helpers/services_helper.rb
+++ contained/management/webinterface/app/helpers/services_helper.rb
@@ -1,2 +1,14 @@
 module ServicesHelper
+  def list(collection, type="elements")
+    if collection.size>0
+      r ="<ul>"
+      collection.each do |e|
+        r += "<li>#{e}</li>"
+      end
+      
+      return r+"</ul>"
+    else
+      "<p class='no_list_elements'>No #{type}</p>"
+    end
+  end
 end
--- contained/management/webinterface/startup.rb
+++ contained/management/webinterface/startup.rb
@@ -3,7 +3,6 @@ ARGV << "-p"; ARGV << "5005"
 require 'config/boot'
 
 class ServiceManager
-  exposed_methods :start
   def start()
     require 'commands/server'
   end
