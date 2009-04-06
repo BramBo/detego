46d4dd55e4314a8ddbd0063de10b475641400151
Bram Wijnands <bram@bram-wijnandss-macbook-pro.local>
2009-04-06 16:52:07 +0200

    small changes man. intf.

--- config/config.rb
+++ config/config.rb
@@ -3,7 +3,7 @@ PATH            = "."
 $: << "#{PATH}/app"
 $: << "#{PATH}/lib"
 SERVICES_PATH   = "#{PATH}/contained"
-DETEGO_VERSION  = 0.1
+DETEGO_VERSION  = "0.1.2"
 
 # DRB Port management
 $port_start   = 15619
