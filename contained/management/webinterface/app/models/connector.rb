0e6f420dbd5202e274d7c0c95018e495af68f8f7
Bram Wijnands <bram@bram-wijnandss-macbook-pro.local>
2009-04-17 14:06:32 +0200

    minor mods search func.

--- contained/management/webinterface/app/models/connector.rb
+++ contained/management/webinterface/app/models/connector.rb
@@ -3,6 +3,13 @@ require 'yaml'
 class Connector
   attr_reader :domains
   
+  def initialize
+    @domains = []
+    $provider.get_domains().each do |n|
+      @domains << Domain.new(n)
+    end
+  end
+  
   def self.domains
     domains = []
     $provider.get_domains().each do |n|
